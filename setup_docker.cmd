7c124e50cebc2abf12b05bcc72d4de39fe0b98a9
Ronny Stauffer <ronny.stauffer@wir-entwickeln.ch>
2016-08-17 09:25:52 +0200

    Add setup_docker script

--- setup_docker.cmd
+++ setup_docker.cmd
@@ -9,4 +9,4 @@ rem If this is not the case: Change IP address here and possibly run 'docker-mac
 set DOCKER_HOST=tcp://192.168.99.100:2376
 set DOCKER_TLS_VERIFY=1
 set DOCKER_CERT_PATH=z:\virtual_machines\docker\machines\default
-rem Note: Kitematic still requires the certificates at path 'c:\users\<User Name>\.docker\machine\machines\default'!
+rem NOTE: Kitematic still requires the certificates at path 'c:\users\<User Name>\.docker\machine\machines\default'!
