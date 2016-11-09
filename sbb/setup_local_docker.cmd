39447909ba97116ade611f54ea599985c3296427
ue56923 <ronny.stauffer@sbb.ch>
2016-11-09 11:35:47 +0100

    Some changes

---
+++ sbb/setup_local_docker.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+set DOCKER_HOST=tcp://192.168.99.100:2376
+set DOCKER_CERT_PATH=C:\Users\ue56923\.docker\machine\machines\default
+set DOCKER_TLS_VERIFY=1
---
+++ sbb/setup_mvdtools_docker.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set DOCKER_TOOLBOX_INSTALL_PATH=c:\progs\docker_toolbox\1.9.1
+
+rem mvdtools.sbb.ch = v00971.sbb.ch
+set DOCKER_HOST=tcp://v00971.sbb.ch:5555
+rem set DOCKER_CERT_PATH=C:\Users\ue56923\.docker\machine\machines\default
+rem set DOCKER_TLS_VERIFY=0
