a28a1e11fa86951010413201c6654c3c9c04b6c9
Ronny Stauffer <ronny.stauffer@sbb.ch>
2018-05-09 09:03:10 +0200

    Some changes

---
+++ _sbb/setup_eaio_openjdk_8.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+set JAVA_HOME=c:\devsbb\eaio-ng\jdk\openjdk8
+set PATH=c:\devsbb\eaio-ng\jdk\openjdk8\bin;%PATH%
---
+++ _sbb/setup_oracle-compatible_jdk.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+set JAVA_HOME=c:\progs\azul_systems_zulu\8.28.0.1
+set PATH=c:\progs\azul_systems_zulu\8.28.0.1\bin;%PATH%
